fd7f2a8537a063e8ff3cafe7e4623c1e67f87020
Viktor Ens <v.ens86@yahoo.de>
2024-04-22 14:11:07 +0200

    add passwordValidator in Main

--- src/main/java/dev/ens/Main.java
+++ src/main/java/dev/ens/Main.java
@@ -1,7 +1,28 @@
 package dev.ens;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+        passwordValidator(password);
+
+    }
+
+
+    public static boolean passwordValidator(String password){
+
+        if( password != null && Password.isPasswordValid(password)){
+            System.out.println("Password: " + password + " is valid");
+            return true;
+        }else{
+            System.out.println("Password: " + password + " is invalid");
+            return false;
+        }
+
+
     }
 }
--- src/test/java/dev/ens/MainTest.java
+++ src/test/java/dev/ens/MainTest.java
@@ -1,4 +1,23 @@
 package dev.ens;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class MainTest {
+
+    @Test
+    void whenPasswordIsNullReturnFalse() {
+        //Given
+        String password = null;
+
+        //Then
+
+        boolean actual = Main.passwordValidator( password );
+
+        //When
+        boolean expected = false;
+        assertFalse( actual, String.valueOf(expected));
+
+    }
 }
